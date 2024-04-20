5d65de48e9bf48d5705e4d2e19deeca166a70323
suk-6 <me@suk.kr>
2024-04-20 21:12:18 +0900

    Add requirements.txt and sentiment.py files

---
+++ sentiment.py
@@ -0,0 +1,30 @@
+from transformers import pipeline, AutoTokenizer, AutoModelForSequenceClassification
+
+
+class Sentiment:
+    def __init__(self):
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            "nlp04/korean_sentiment_analysis_dataset3"
+        )
+
+        self.model = AutoModelForSequenceClassification.from_pretrained(
+            "nlp04/korean_sentiment_analysis_dataset3"
+        )
+
+        self.classifier = pipeline(
+            "text-classification",
+            model=self.model,
+            tokenizer=self.tokenizer,
+            device="cpu",
+            top_k=None,
+        )
+
+    def __call__(self, text: str):
+        return self.classifier(text)
+
+
+if __name__ == "__main__":
+    sentiment = Sentiment()
+    text = "이 영화는 정말 재미있었어!"
+    result = sentiment(text)
+    print(result)
