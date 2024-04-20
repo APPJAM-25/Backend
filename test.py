8f3adc87bdf0d2b6ba49503c03db0720ed26561e
suk-6 <me@suk.kr>
2024-04-20 20:36:15 +0900

    Remove commented out code for Content-Type header

--- test.py
+++ test.py
@@ -4,7 +4,6 @@ with open("test.wav", "rb") as f:
     result = requests.post(
         "http://localhost:8000/chat/asdf",
         files={"file": f},
-        # headers={"Content-Type": "audio/wav"},
     )
 
     print(result.json())
