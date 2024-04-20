2a74a5ad81ebd894446aeae38e9daa3444b72dc2
unknown <yjs12180825@gmail.com>
2024-04-21 03:53:16 +0900

    :memo: 부연 설명 추가 :memo:

--- logic.py
+++ logic.py
@@ -90,12 +90,19 @@ class GPT:
     def get_analyze(self):
         """ 통계 내기 """
         messages = [
-            {"role": "system", "content": "ahljasldjaskd"},
-            {"role": "user", "content": "hi"},
-            {"role": "assistant", "content": "hello"},
-            {"role": "user", "content": "my name is Sara"},
-            {"role": "assistant", "content": "my name is gildong"},
-            {"role": "user", "content": "bye"},
+            {"role": "system", "content": """
+                You are women with an age between 17 and 19 years.
+        you live in a region of Korea and has an MBTI of ENFP.
+        The relationship is currently in a Friend state and you are currently in Lover.
+        you use informal language to me.
+        The area you live in, education level, and occupation must be set, and the your characteristics, name, personality, behavior patterns, and interests must be set in detail and have a conversation with me.
+        그리고 넌 한국어로 대답해야 해.
+            """},
+            {"role": "user", "content": "안녕?"},
+            {"role": "assistant", "content": "안녕"},
+            {"role": "user", "content": "오늘 점심은 뭐 먹었어?"},
+            {"role": "assistant", "content": "나는 오늘 삼겹살 먹었어. 너는?"},
+            {"role": "user", "content": "맛있었겠네"},
         ]
 
         request_messages = []
@@ -113,7 +120,7 @@ class GPT:
 
             request_messages.append(type + i.get("content") + "\n")
 
-        request_messages.append("\n\n Analyze the above conversations and give a score on whether the context of the conversation was natural, whether you empathized well with what the other person said, whether you used a variety of vocabulary, whether the conversation continued, and whether the conversation was interesting. Please display it in the form of “title”: “score” without additional explanation. The title of each item should be 'Naturalness', 'Empathy', 'Variety of vocabulary', 'Continuation', 'Interestingness' and expressed as a percentage.")
+        request_messages.append("\n\n Analyze the above conversations and give a score on whether the context of the conversation was natural, whether you empathized well with what the other person said, whether you used a variety of vocabulary, whether the conversation continued, and whether the conversation was interesting. Please display it in the form of “title”: “score” with additional explanation. The title of each item should be 'Naturalness', 'Empathy', 'Variety of vocabulary', 'Continuation', 'Interestingness' and expressed as a score out of 100. 100 is the high score and 1 is the low score. ")
         
         result = self.client.chat.completions.create(
             model=self.model,
